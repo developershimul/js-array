0bbf9d97476ae65e75001fb548b840be28aed9af
Developer Shimul <developershimul0@gmail.com>
2024-10-26 23:05:28 +0600

    array value updated

--- array.js
+++ array.js
@@ -1,3 +1,11 @@
 const numbers = [10, 12, 58, 69, 47, 156, 548, 458, 15];
 console.log(numbers.length);
 console.log(numbers[6]);
+console.log(numbers[numbers.length - 1]);
+numbers[5] = 105;
+console.log(numbers);
+console.log(numbers.includes(156));
+numbers.push(4569);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
