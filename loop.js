ad63fc1457dcfae333681660bc3df31aed02f28f
Developer Shimul <developershimul0@gmail.com>
2024-10-29 20:46:54 +0600

    Code update

--- loop.js
+++ loop.js
@@ -1,10 +1,10 @@
-//for loop
-// for (let x = 0; x <= 10; x += 1) {
-//   console.log(x);
-// }
-// for (let i = 1; i <= 100; i = i + 2) {
-//   console.log(i);
-// }
+for loop
+for (let x = 0; x <= 10; x += 1) {
+  console.log(x);
+}
+for (let i = 1; i <= 100; i = i + 2) {
+  console.log(i);
+}
 
 let numbers = [10, 20, 30, 40, 50, 60, 70];
 let doubleNumbers = [];
@@ -14,3 +14,16 @@ for (let x = 0; x <= numbers.length - 1; x++) {
   doubleNumbers.push(multiplyNum);
 }
 console.log(doubleNumbers);
+
+let friends = ["Akash", "Batas", "Jalal", "Sabbir", "Labu"];
+console.log(friends);
+for (let friend of friends) {
+  console.log(friend);
+}
+console.log(Array.isArray(numbers));
+let shortNum = [];
+for (let num of numbers) {
+  let mulNum = num * 3;
+  numbers.push(mulNum);
+}
+console.log(shortNum);
